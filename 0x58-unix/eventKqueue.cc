fed56d5f443797de5cefa76479ffd5743939638c
Bert JW Regeer <bertjw@regeer.org>
2008-10-03 10:43:56 +0000

    Update the license to the cleaner and smaller OpenBSD license. It does the same thing as before, just with less words and thus less space taken up.
    This is an event implementation for kqueue for Mac OS X and FreeBSD. As of right now it does not yet return any useful data. I have yet to decide on whether or not I want all callback "classes" to have to derive from one base class, or if I want to do function pointers and let the programmer on the other end deal with keeping certain information around. Function pointers sound pretty good to me though, so I may go that route...
    
    
    
    git-svn-id: http://svn.0x58.com/repos/bsdpanel/0x58-unix/trunk@221 db258d53-41f1-0310-98c4-cc461a13e28c

--- 0x58-unix/eventKqueue.cc
+++ 0x58-unix/eventKqueue.cc
@@ -1,93 +1,96 @@
 /**
- * Copyright 2008 Bert JW Regeer. All rights  reserved.
+ * Copyright (c) 2008 Bert JW Regeer <xistence@0x58.com>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and  documentation are
- * those of the authors and should not be  interpreted as representing official
- * policies, either expressed  or implied, of bsdPanel project.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
 **/
 
 #include <0x58-unix/eventKqueue.h>
+namespace x58unix {
+        namespace eventImplementation {
+                eventKqueue::eventKqueue() {
+                        _kq = kqueue();
+                }
 
-x58unix::eventImplementation::eventKqueue::eventKqueue() {
-        _kq = kqueue();
-}
+                eventKqueue::~eventKqueue() {
+                        close(_kq);
+                }
 
-x58unix::eventImplementation::eventKqueue::~eventKqueue() {
-        close(_kq);
-}
+                bool eventKqueue::_addEvent(uintptr_t ident, short filter, u_short flags, u_int fflags, intptr_t data, void *udata) {
+                        // This is the new kevent we are going to add to the queue
+                        struct kevent change;
+        
+                        // We need a timespec that is going to let us return immediatly
+                        struct timespec ts;
+                        ts.tv_sec = 0;
+                        ts.tv_nsec = 0;
+        
+                        // Add the event, enable the event, and make sure that each time through we clear the state of the event.
+                        flags |= (EV_ADD | EV_ENABLE | EV_CLEAR);
+        
+                        // Use a macro to fill kevent.
+                        /**
+                                TODO Change this from the macro to actual code? Might be beneficial.
+                        **/
+        
+                        EV_SET(&change, ident, filter, flags, fflags, data, udata);
+        
+                        // Add the event to the kqueue, we tell it we just want to add 1 and immediatly return using the timespec we have set up
+                        if (kevent(_kq, &change, 1, 0, 0, &ts) < 0) {
+                                // Failure. Return false
+                                perror("kqueue: ");
+                                return false;
+                        }
+        
+                        return true;
+                }
 
-bool x58unix::eventImplementation::eventKqueue::_addEvent(uintptr_t ident, short filter, u_short flags, u_int fflags, intptr_t data, void *udata) {
-        // This is the new kevent we are going to add to the queue
-        struct kevent change;
+                bool eventKqueue::read(int fd, bool oneshot, void * cb) 
+                {
+                        // Create the flags short, this is going to contain the flags we want to enable
+                        unsigned short flags = 0;
+                        if (oneshot) flags |= EV_ONESHOT;
         
-        // We need a timespec that is going to let us return immediatly
-        struct timespec ts;
-        ts.tv_sec = 0;
-        ts.tv_nsec = 0;
-        
-        // Use a macro to fill kevent
-        EV_SET(&change, ident, filter, flags, fflags, data, udata);
-        
-        // Add the event to the kqueue, we tell it we just want to add 1 and immediatly return using the timespec we have set up
-        if (kevent(_kq, &change, 1, 0, 0, &ts) < 0) {
-                // Failure. Return false
-                perror("kqueue: ");
-                return false;
-        }
-        
-        return true;
-}
+                        return _addEvent(fd, EVFILT_READ, flags, 0, 0, (void *)cb);
 
-bool x58unix::eventImplementation::eventKqueue::read(void * cb, int fd, bool oneshot, bool clear) 
-{
-        unsigned int flags = EV_ADD | EV_ENABLE;
-        if (oneshot) flags |= EV_ONESHOT;
-        if (clear)   flags |= EV_CLEAR;
-        
-        return _addEvent(fd, EVFILT_READ, flags, 0, 0, (void *)cb);
+                }
 
-}
-
-bool x58unix::eventImplementation::eventKqueue::write(void * cb, int fd, bool oneshot, bool clear) 
-{
-        unsigned int flags = EV_ADD | EV_ENABLE;
-        if (oneshot) flags |= EV_ONESHOT;
-        if (clear)   flags |= EV_CLEAR;
+                bool eventKqueue::write(int fd, bool oneshot, void * cb) 
+                {
+                        unsigned short flags = 0;
+                        if (oneshot) flags |= EV_ONESHOT;
         
-        return _addEvent(fd, EVFILT_WRITE, flags, 0, 0, (void *)cb);
-}
+                        return _addEvent(fd, EVFILT_WRITE, flags, 0, 0, (void *)cb);
+                }
 
-void x58unix::eventImplementation::eventKqueue::dispatch() {
-        struct kevent event;
+                void eventKqueue::dispatch() {
+                        /**
+                                FIXME Actually have the function return the kevents. Choose between function pointers or having a seperate class
+                        **/
         
-        while (1) {
-                int nev = kevent(_kq, 0, 0, &event, 1, 0);
+                        struct kevent event;
+        
+                        while (1) {
+                                int nev = kevent(_kq, 0, 0, &event, 1, 0);
                 
-                if (nev == -1) {
-                        perror("kqueue: ");
-                        throw x58unix::DispatchFailed();
+                                if (nev == -1) {
+                                        perror("kqueue: ");
+                                        throw x58unix::DispatchFailed();
+                                }
+                        }
                 }
         }
 }
+
+// We enroll the event handler in the roll call.
+ENROLL_EVENT(kqueue, x58unix::eventImplementation::eventKqueue);
